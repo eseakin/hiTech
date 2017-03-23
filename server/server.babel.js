a6e35530f4fa6fc1e477f01c0b3e7fa92c354634
Eric Eakin <eseakin@gmail.com>
2017-03-22 21:56:40 -0700

    Tweaking to get heroku to work

--- server/server.babel.js
+++ server/server.babel.js
@@ -3,6 +3,6 @@ import express from 'express';
 const app = express();
 
 app.use('/', express.static('public'));
-
-app.listen(process.env.PORT || 3000);
-console.log('listening on ', 3000)
+const port = process.env.PORT || 3000
+app.listen(port);
+console.log('listening on', port)
