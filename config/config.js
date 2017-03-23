0ba0f7bedbcb8b73fead0785d732d3d6554d26bd
Eric Eakin <eseakin@gmail.com>
2017-03-22 22:01:09 -0700

    Added config for heroku

---
+++ config/config.js
@@ -0,0 +1,9 @@
+const config = {
+  apiKey: "AIzaSyAfaox1W-3L4YSy24KnSI8zxXnDY3NJtBg",
+  authDomain: "hitech-799b7.firebaseapp.com",
+  databaseURL: "https://hitech-799b7.firebaseio.com",
+  storageBucket: "hitech-799b7.appspot.com",
+  messagingSenderId: "732161124904"
+};
+
+export default config
