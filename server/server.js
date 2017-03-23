ec502fcbdd388be232039c944f1622a14d9a7be7
Eric Eakin <eseakin@gmail.com>
2017-03-22 21:12:30 -0700

    Added empty line at end of every file.

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('../webpack.config.js');
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
-const port = isDeveloping ? 3000 : process.env.PORT;
+// const port = isDeveloping ? 3000 : process.env.PORT;
+port=3001
 const app = express();
 
 if (isDeveloping) {
@@ -29,13 +30,13 @@ if (isDeveloping) {
   app.use(middleware);
   app.use(webpackHotMiddleware(compiler));
   app.get('*', function response(req, res) {
-    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, '../dist/index.html')));
     res.end();
   });
 } else {
-  app.use(express.static(__dirname + '/dist'));
+  app.use(express.static(__dirname + '../dist'));
   app.get('*', function response(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
   });
 }
 
